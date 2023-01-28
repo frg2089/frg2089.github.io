6f927430fe0312a48edd152f6fd4c600ef95ccd6
MWX <37146632+mwx2006@users.noreply.github.com>
2023-01-28 22:53:38 +0800

    Update vuepress.friends.ts

--- vuepress.friends.ts
+++ vuepress.friends.ts
@@ -6,5 +6,11 @@ export const friends: FriendLink[] = [
     logo: 'https://sora.ink/wp-content/uploads/2021/04/icon.png',
     desc: '是DC姐姐',
     link: 'https://sora.ink/',
+  },
+  {
+    name: "MWXの花田",
+    logo: "https://dkrain.com/assets/img/auther.jpg",
+    desc: "MWX!",
+    link: "https://dkrain.com"
   }
 ]
