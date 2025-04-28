68474a76cd02c7658d4cd359b41d73ca3ba1530c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-04-29 06:40:20 +0800

    feat(theme): 更新页脚内容与版权信息
    - 将页脚信息更改为备案号链接
    - 添加版权信息，包括知识共享许可协议和年度版权声明
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- vuepress.theme.ts
+++ vuepress.theme.ts
@@ -40,7 +40,12 @@ export default hopeTheme(
     displayFooter: true,
 
     footer:
-      '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
+      '<a href="https://icp.gov.moe/?keyword=20252089" target="_blank">萌ICP备20252089号</a>',
+    copyright: [
+      '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" /></a>',
+      '本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议</a>进行许可。',
+      `Copyright © ${new Date().getFullYear()} 舰队的偶像-岛风酱!`,
+    ].join('<br />'),
 
     markdown: {
       gfm: true,
