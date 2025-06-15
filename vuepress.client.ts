6e0a2c2f0b71f0ab32c099518da6daf9b603a50f
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-06-15 20:47:58 +0800

    style(docs): 调整页面宽度并移除博客页面样式
    - 将页面宽度从 48rem 调整为 50rem
    - 移除了博客页面的冗余样式代码
    - 添加了圆角博主头像样式
    
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- vuepress.client.ts
+++ vuepress.client.ts
@@ -1,6 +1,8 @@
 import { defineClientConfig } from 'vuepress/client'
 import FriendsHome from './src/layouts/FriendsHome.vue'
 
+import 'vuepress-theme-hope/presets/round-blogger-avatar.scss'
+
 export default defineClientConfig({
   layouts: { FriendsHome },
 })
