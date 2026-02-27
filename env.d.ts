439ad25f5a5d7b85cfbcf019bdaf4fbb4f42c185
舰队的偶像-岛风酱 <frg2089@outlook.com>
2026-02-27 15:41:55 +0800

    chore: 移除VuePress相关依赖和脚本
    Signed-off-by: 舰队的偶像-岛风酱 <frg2089@outlook.com>

--- env.d.ts
+++
@@ -1,5 +0,0 @@
-type FriendLink = import('vuepress-theme-hope').ThemeBlogHomeProjectOptions & {
-  group?: string
-}
-
-declare const しまかぜのともだち: FriendLink[]
--- vuepress.client.ts
+++
@@ -1,8 +0,0 @@
-import { defineClientConfig } from 'vuepress/client'
-import FriendsHome from './src/layouts/FriendsHome.vue'
-
-import 'vuepress-theme-hope/presets/round-blogger-avatar.scss'
-
-export default defineClientConfig({
-  layouts: { FriendsHome },
-})
